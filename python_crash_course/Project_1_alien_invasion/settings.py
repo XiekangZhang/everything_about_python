3f361f2fb3f67a19d895a425a0bff73b9e387485
XiekangZhang <zhangxiekang@gmail.com>
2022-09-26 20:32:49 +0200

    b

--- python_crash_course/Project_1_alien_invasion/settings.py
+++ python_crash_course/Project_1_alien_invasion/settings.py
@@ -28,7 +28,7 @@ class Settings:
         self.ship_limit = 3
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -39,3 +39,9 @@ class Settings:
 
         # fleet_direction of 1 represents right, -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
