dcba5cbf48d9511c016c8d4bb739170ece0e031a
Xiekang Zhang <xiekang.zhang@cimt-ag.de>
2022-10-28 16:09:01 +0200

    add something new

--- python_crash_course/Project_2_data_visualization/death_valley_highs_lows.py
+++ python_crash_course/Project_2_data_visualization/death_valley_highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    filename = "data/sitka_weather_2018_simple.csv"
+    filename = "data/death_valley_2018_simple.csv"
     with open(filename) as f:
         reader = csv.reader(f)
         header_row = next(reader)
@@ -14,9 +14,16 @@ if __name__ == "__main__":
         # Get dates and high temperatures from this file.
         dates, highs, lows = [], [], []
         for row in reader:
-            highs.append(int(row[5]))
-            lows.append(int(row[6]))
-            dates.append(datetime.strptime(row[2], "%Y-%m-%d"))
+            current_date = datetime.strptime(row[2], "%Y-%m-%d")
+            try:
+                high = int(row[4])
+                low = int(row[5])
+            except ValueError:
+                print(f"Missing data for {current_date}")
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
 
     # Plot the high and low temperatures
     plt.style.use("seaborn-v0_8")
@@ -27,9 +34,9 @@ if __name__ == "__main__":
     # INFO: fill shading area between two y_values --> fill_between(x_values, y_values1, y_values2, ...)
     ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
     # Format plot
-    ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
+    ax.set_title("Daily high and low temperatures - 2018\nDeath Valley, CA", fontsize=20)
     ax.set_xlabel("", fontsize=16)
-    fig.autofmt_xdate() # INFO: draws the data labels diagonally to prevent them from overlapping
+    fig.autofmt_xdate()  # INFO: draws the data labels diagonally to prevent them from overlapping
     ax.set_ylabel("Temperature (F)", fontsize=16)
     ax.tick_params(axis="both", which="major", labelsize=16)
     plt.show()
