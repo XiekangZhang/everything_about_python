bbecd1c91bd49b8b29901c4cef6e00e6455b4415
XiekangZhang <zhangxiekang@gmail.com>
2022-11-01 19:58:30 +0100

    finish project 2

--- python_crash_course/Project_2_data_visualization/python_repos_visual.py
+++ python_crash_course/Project_2_data_visualization/python_repos_visual.py
@@ -4,7 +4,8 @@ from plotly import offline
 
 if __name__ == "__main__":
     # Make an API call and store the response
-    url = "https://api.github.com/search/repositories?q=language:c++&sort=stars"
+    # IDEA: JavaScript, Ruby, C/C++, Java, Perl, Haskell, GO
+    url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
     headers = {"Accept": "application/vnd.github.v3+json"}
     r = requests.get(url, headers=headers)
     print(f"Status code: {r.status_code}")
