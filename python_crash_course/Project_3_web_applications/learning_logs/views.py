fcaa49c29c33dc5ee2aeb51bd6fd84243894440d
XiekangZhang <zhangxiekang@gmail.com>
2022-12-06 20:46:05 +0100

    finish form part

--- python_crash_course/Project_3_web_applications/learning_logs/views.py
+++ python_crash_course/Project_3_web_applications/learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -43,10 +43,11 @@ def new_topic(request):
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
 
+
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
-    if request.method!= "POST":
+    if request.method != "POST":
         # No data submitted; create a blank form.
         form = EntryForm()
     else:
@@ -61,3 +62,21 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+    context = {"form": form, "topic": topic, "entry": entry}
+    return render(request, "learning_logs/edit_entry.html", context)
