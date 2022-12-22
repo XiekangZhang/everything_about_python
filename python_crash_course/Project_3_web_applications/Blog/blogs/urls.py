1ea8d6fa8a6b349e0048fa1c1c4a358f4d2addfc
XiekangZhang <zhangxiekang@gmail.com>
2022-12-22 12:04:36 +0100

    cleaning code

--- python_crash_course/Project_3_web_applications/Blog/blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("add_blog/", views.add_blog, name="add_blog"),
-    path("edit_blog/<int:blog_id>/", views.edit_blog, name="edit_blog")
-]
