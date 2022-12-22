1d3ce657802258d484c100ee28016dd05e74108d
XiekangZhang <zhangxiekang@gmail.com>
2022-12-22 13:59:36 +0100

    finishing registration

--- python_crash_course/Project_3_web_applications/Blog/Blog/settings.py
+++ python_crash_course/Project_3_web_applications/Blog/Blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Blog.blogs"
+    "Blog.blogs",
+    "Blog.users",
 ]
 
 MIDDLEWARE = [
