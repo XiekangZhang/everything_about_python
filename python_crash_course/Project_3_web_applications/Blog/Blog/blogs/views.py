f2f7b60b93e06b99308d7e72fba5c364bd160581
XiekangZhang <zhangxiekang@gmail.com>
2022-12-23 15:32:06 +0100

    finish tasks

--- python_crash_course/Project_3_web_applications/Blog/Blog/blogs/views.py
+++ python_crash_course/Project_3_web_applications/Blog/Blog/blogs/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import BlogPost
 from .forms import BlogForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -8,14 +10,17 @@ def index(request):
     return render(request, 'blogs/index.html')
 
 
+@login_required
 def blogs(request):
     # show all blogs
     # blogs = BlogPost.objects.all()
-    blogs = BlogPost.objects.order_by("date_added")
+    # blogs = BlogPost.objects.order_by("date_added")
+    blogs = BlogPost.objects.filter(owner=request.user).order_by("date_added")
     context = {'blogs': blogs}
     return render(request, 'blogs/blogs.html', context)
 
 
+@login_required
 def new_blog(request):
     # add a new blog post
     if request.method != "POST":
@@ -23,20 +28,29 @@ def new_blog(request):
     elif request.method == "POST":
         blogForm = BlogForm(request.POST)
         if blogForm.is_valid():
-            blogForm.save()
+            blog = blogForm.save(commit=False)
+            blog.owner = request.user
+            blog.save()
             return redirect("blogs:blogs")
     context = {"form": blogForm}
     return render(request, 'blogs/new_blog.html', context)
 
 
+@login_required
 def blog(request, blog_id):
     blog = BlogPost.objects.get(id=blog_id)
+    if blog.owner != request.user:
+        raise Http404
     context = {'blog': blog}
     return render(request, 'blogs/blog.html', context)
 
 
+@login_required
 def edit_blog(request, blog_id):
     blog = BlogPost.objects.get(id=blog_id)
+    if blog.owner != request.user:
+        raise Http404
+
     if request.method != 'POST':
         blogForm = BlogForm(instance=blog)
     else:
