c09fb0b4613059aa5a63908fb2ff5eac44931c4c
Zhang <Xiekang.Zhang@tedi.com>
2025-06-17 14:19:20 +0200

    clean deprecated files

--- playground/python_crash_course/PART_1/battery.py
+++
@@ -1,9 +0,0 @@
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
-    def __init__(self, battery_size=75):
-           """Initialize the battery's attributes."""
-           self.battery_size = battery_size
-
-    def describe_battery(self):
-           """Print a statement describing the battery size."""
-           print(f"This car has a {self.battery_size}-kWh battery.")
--- playground/python_crash_course/PART_1/name_function.py
+++
@@ -1,3 +0,0 @@
-def get_formatted_name(first: str, last: str) -> str:
-    full_name = f"{first} {last}"
-    return full_name.title()
